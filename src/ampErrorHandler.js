483098fdab66592e33992d4fe6c0c34ce5cad47f
Mark Madden <markmadden@Marks-Mac-mini.local>
2018-03-14 23:21:51 -0500

    added comment

--- src/ampErrorHandler.js
+++ src/ampErrorHandler.js
@@ -3,5 +3,7 @@ var validationErrors = require('../validator/validationErrors');
 
 function handleErrors(content,ampMessage){
         console.log(ampMessage);
+        //check to see if incoming error is in json
+        //if match, do fix
 }
 module.exports = handleErrors;
